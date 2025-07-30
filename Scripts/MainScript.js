825b41123892cbad62aad4fd7369e165260419e0
kevin Foendoe <kfoendoe1@gmail.com>
2025-07-30 07:26:38 -0400

    added car options

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -3,9 +3,30 @@ var log = console.log;
 const yearSelect = document.getElementById("YearPicker");
 const currentYear = new Date().getFullYear();
 
-for (let year = 2000; year <= currentYear + 1; year++) {
-    const option = document.createElement("option");
-    option.value = year;
-    option.textContent = year;
-    yearSelect.appendChild(option);
+//
+let currentCar = "/Media/Cars/sedan.png";
+$("#Car").attr("src", currentCar);
+
+function updateOptions() {
+    $(".Car_Option").each(function () {
+        const optionSrc = $(this).data("src");
+        if (optionSrc === currentCar) {
+            $(this).hide();
+        } else {
+            $(this).show();
+        }
+    });
 }
+
+updateOptions();
+
+$(document).on("click", ".Car_Option", function () {
+    const clickedSrc = $(this).data("src");
+    currentCar = clickedSrc;
+    $("#Car").attr("src", currentCar);
+    $(".Car_Option").removeClass("selected");
+    $(this).addClass("selected");
+    updateOptions();
+});
+
+
